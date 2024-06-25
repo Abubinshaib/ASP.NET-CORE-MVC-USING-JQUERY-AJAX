16ac44f7a51284a0c2343b5a2761529dc3343c7d
Ameerkhan <110119104009@aalimec.ac.in>
2024-06-25 15:03:40 +0530

    Add project files.

---
+++ ASP.NET CORE MVC USING JQUERY AJAX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_CORE_MVC_USING_JQUERY_AJAX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
